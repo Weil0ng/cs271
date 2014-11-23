455de6256dc073f531afd1e6e1de137c899f860d
cuiweilong0205@gmail.com <cuiweilong0205@gmail.com>
2014-11-22 16:37:08 -0800

    commit

--- CmdInterpreter.py
+++ CmdInterpreter.py
@@ -16,23 +16,23 @@ CONN = [None] * len(IP)
 def queryServer(index):
     while True:
         try:
-    	    print "Querying server %d" % index
+    	    print ("Querying server %d" % index)
 	    OUT_SOCK[index].connect((IP[index], PORT[index]))
-	    print "Connect established"
+	    print ("Connect established")
 	    break;
         except:
-	    print "QueryServer Exception"
-	    time.sleep(2)
+	    print ("QueryServer Exception")
+	    time.sleep(1)
 
 def waitForClient(index):
-	print "Waiting for client %d" % index
+	print ("Waiting for client %d" % index)
 	IN_SOCK[index].bind(('0.0.0.0', 42709))
 	IN_SOCK[index].listen(1)
         CONN[index], addr = IN_SOCK[index].accept()
 	print addr
 
 def init_conn():
-    print "Initializing connection..."
+    print ("Initializing connection...")
     index = 0
     while index < len(IP):
     	OUT_SOCK[index] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -44,32 +44,34 @@ def init_conn():
 	queryServer(i)
 
 def send2Server(msg, index):
+    print ("sending $s" % msg)
     OUT_SOCK[index].send(msg)
 
 class CmdInterpreter(cmd.Cmd):
 
     def do_deposit(self, arg):
-	print arg
+	print (arg)
 
     def do_withdraw(self, arg):
-	print "withdraw"
+	print ("withdraw")
     
     def do_balance(self, arg):
-	print "balance"
+	print ("balance")
     
     def do_fail(self, arg):
-        print "fail a node"
+        print ("fail a node")
 
     def do_unfail(self, arg):
-	print "recover a node"    
+	print ("recover a node")
 
     def do_EOF(self, arg):
-	return True
+	return (True)
 
     def postloop(self):
-	print "Goodbye!"
+	print ("Goodbye!")
 
 if __name__ == '__main__':
     init_conn()
+    send2Server("hello", 0)
     cmdInterp = CmdInterpreter()
     cmdInterp.cmdloop("Please enter cmd: \n\tdeposit [number] \n\twithdraw [number] \n\tbalance \n\tfail \n\tunfail \npress CTRL+D to quit.")
