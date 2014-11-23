8446ba2dd95de3da47ec59b247ec7a62a7500957
cuiweilong0205@gmail.com <cuiweilong0205@gmail.com>
2014-11-22 16:10:29 -0800

    init commit

---
+++ CmdInterpreter.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import cmd
+import readline
+import socket
+import thread
+import time
+
+log = []
+IP = ["169.231.6.155"]
+PORT = [12345]
+OUT_SOCK = [None] * len(IP)
+IN_SOCK = [None] * len(IP)
+CONN = [None] * len(IP)
+
+def queryServer(index):
+    while True:
+        try:
+    	    print "Querying server %d" % index
+	    OUT_SOCK[index].connect((IP[index], PORT[index]))
+        except:
+	    print "QueryServer Exception"
+	    time.sleep(2)
+
+def waitForClient(index):
+	print "Waiting for client %d" % index
+	IN_SOCK[index].bind(('0.0.0.0', 42709))
+	IN_SOCK[index].listen(1)
+        CONN[index], addr = IN_SOCK[index].accept()
+	print addr
+
+def init_conn():
+    print "Initializing connection..."
+    index = 0
+    while index < len(IP):
+    	OUT_SOCK[index] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	IN_SOCK[index] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	index += 1
+    for i in range(0, len(IP)):
+	thread.start_new_thread(waitForClient, (i, ))
+    for i in range(0, len(IP)):
+	thread.start_new_thread(queryServer, (i, ))
+	
+
+class CmdInterpreter(cmd.Cmd):
+
+    def do_deposit(self, arg):
+	print arg
+
+    def do_withdraw(self, arg):
+	print "withdraw"
+    
+    def do_balance(self, arg):
+	print "balance"
+    
+    def do_fail(self, arg):
+        print "fail a node"
+
+    def do_unfail(self, arg):
+	print "recover a node"    
+
+    def do_EOF(self, arg):
+	return True
+
+    def postloop(self):
+	print "Goodbye!"
+
+if __name__ == '__main__':
+    init_conn()
+    cmdInterp = CmdInterpreter()
+    cmdInterp.cmdloop("Please enter cmd: \n\tdeposit [number] \n\twithdraw [number] \n\tbalance \n\tfail \n\tunfail \npress CTRL+D to quit.")
